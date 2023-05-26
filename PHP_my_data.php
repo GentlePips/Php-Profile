3edaee8f87d01b762bb402710444c6755048542a
Okafor Humphrey Ikechukwu <133217957+GentlePips@users.noreply.github.com>
2023-05-26 14:25:10 -0700

    Add files via upload

---
+++ PHP_my_data.php
@@ -0,0 +1,11 @@
+<?php
+$name = "<h1>OKAFOR HUMPHREY IKECHUKWU</h1>";
+$regno = "2021/HND/33802/CS<br>";
+$soo = "IMO STATE<br>";
+$project = "<strong>DESIGN AND IMPEMENTATION OF AN AUTHOMATED CHATBOT FOR
+PRICE NEGOTIATION</strong>";
+echo $name;
+echo $regno;
+echo $soo;
+echo $project;
+?>
